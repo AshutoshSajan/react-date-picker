5858bbfb2b64c88baf5fb8ab592dc3bda6382257
sajan <sajanloveu123@gmail.com>
2019-06-20 15:24:33 +0530

    add select month functionality

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ class App extends Component {
 
   state = {
     date: "",
-    type: "",
     icon: true,
     calender: false,
+    error: "",
+    sendDate: false
   }
 
   // handleClick = () => {}
 
   mouseEnter = () => {
-    this.setState({ icon: !this.state.icon });
+    if(this.state.date){
+      this.setState({ icon: !this.state.icon });
+    }
   }
 
   handleMouseLeave = () => {
@@ -22,33 +25,52 @@ class App extends Component {
   }
 
   handleClear = () => {
-    this.setState({ date: "" });
+    this.setState({ icon: true, date: "" });
   }
 
   handleFocus = () => {
     this.setState({ calender: true });
   }
 
-  handleBlur = () => {
-    this.setState({ calender: false });
-  }
+  // handleBlur = () => {
+  //   this.setState({ calender: false });
+  // }
 
   today = (data, type) => {
     this.setState({ date: data });
   }
 
+  handleChage = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value })
+  }
+
+  handleEnter = (e) => {
+    // console.log(e.keyCode, e.target.placeholder, "chage fired...");
+    if(e.keyCode === 13 && e.target.value.trim()){
+      var a = e.target.value.split("/");
+      if(a.length === 3){
+        // console.log(a,'a...');
+        if((a[0].length <= 2 && Number(a[0]) <= 31) && (a[1].length <= 2 && Number(a[1]) <= 12) && a[2].length === 4){
+          this.setState({ date: e.target.value, inputDate: e.target.value });
+        } else this.setState({ date: "" , error: "invalid date format", sendDate: true });
+      };
+    };
+  }
+
   render() {
+    // console.log(this.state);
     return (
       <div className="app" onFocus={this.handleFocus} /*onBlur={this.handleBlur}*/ >
         <div className="input-box" onMouseEnter={this.mouseEnter} onMouseLeave={this.handleMouseLeave} >
-          <input type={this.state.type || "text" } className="main-input" placeholder="Select Date" value={ this.state.date } readOnly/>
+          <input type="text" className={ this.state.error ? "error main-input" : "main-input" } placeholder={ this.state.error ||"Select Date"} name="date" value={ this.state.date } onChange={ this.handleChage } onKeyDown={ this.handleEnter } />
             {
               this.state.icon ? <i className="far fa-calendar"></i>
               : <i className="fas fa-times-circle" onClick={this.handleClear}></i>
             }
         </div>
         {
-          this.state.calender ?  <Calender today={this.today} /> : null
+          this.state.calender ? <Calender today={this.today} date={ this.state.date } /> : null
         }
 
       </div>
--- src/Calender.js
+++ src/Calender.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
+import DateFormat from './DateFormat';
 
 class Calender extends Component {
 
 	constructor(props){
 		super(props);
+		console.log(this.props.date, "date prop...");
+		// this.dateFromInput = this.props.date;
+		// console.log(this.dateFromInput, "prop cal...");
 		this.date = new Date();
+		this.febDays = "";
 		this.state = {
-			day: this.date.getDay(),
-			month: this.date.getMonth(),
-			year: this.date.getFullYear(),
-			weekDays: ["MON", "TUE", "WED","THU","FRI", "SAT", "SUN"],
-			months: [
-				'January',
-				'February',
-				'March',
-				'April',
-				'May',
-				'June',
-				'July',
-				'August',
-				'September',
-				'October',
-				'November',
-				'December'
-			],
+			day: this.props.date ? this.props.date.split('/')[1] : this.date.getDay(),
+			month: this.props.date ? this.props.date.split('/')[0] : this.date.getMonth() + 1,
+			year: this.props.date ? this.props.date.split('/')[2]  : this.date.getFullYear(),
+			weekDays: ["SUN","MON", "TUE", "WED","THU","FRI", "SAT"],
+			months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+			daysOfMonth: ["31", `${this.febDays || "" }`, "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"],
+
 			today: `${this.date.getFullYear()}/${ this.date.getMonth().toString().length < 2 ? "0" + this.date.getMonth(): this.date.getMonth() }/${ this.date.getDay().toString().length < 2 ? "0" + this.date.getDay() : this.date.getDay() }`,
 			tableCells: 42,
-			days : this.getMonthDays(this.date.getFullYear(), this.date.getMonth()),
-			selectedDay: '',
-			active: "active"
+			days : this.getMonthDays(this.date.getFullYear(), this.date.getMonth() + 1),
+			selectedDay: this.props.date,
+			active: "active",
+			showMonth: false
 		}
 	}
+
+	// componentDidMount() {
+	// 	console.log(this.state.month, "componentDidMount fired...");
+
+	// 	if (this.state.month === 2) {
+ //      if ((this.state.year % 100 !== 0) && (this.state.year % 4 === 0) || (this.state.year % 400 === 0)) {
+ //        this.febDays = 29;
+ //      } else {
+ //        this.febDays = 28;
+ //      }
+ //    }else console.log("month is not == 2");
+	// }
 
 	handleClick = (e) => {
 		if(e.target.dataset.key === "dec-year"){
@@ -52,15 +59,14 @@ class Calender extends Component {
 		}else	return null;
 	}
 
-	getMonthDays = (year, month) => {
-    var date = new Date(year, month + 1, 0);
-    return date.getDate();
-	}
-
-	firstDay = () => {
-		const { year, month } = this.state;
-		var firstDayOfMonth = new Date(year, month, 1);
-		return firstDayOfMonth.toDateString();
+	getMonthDays = (year, month, num = 0) => {
+		if(num === 0){
+			var date = new Date(year, month, num);
+    	return date.getDate();
+		} else if (num === 1){
+			var date = new Date(year, month, num);
+    	return date.toDateString();
+		} else { return null };
 	}
 
 	createTable = (num) => {
@@ -82,20 +88,44 @@ class Calender extends Component {
 		this.props.today(selectedDay);
 	}
 
+	handleMonth = () => {
+		this.setState({ showMonth: !this.state.showMonth });
+	}
+
+	selectMonth = (e) => {
+		const { month, months, year, selectedDay, showMonth } = this.state;
+		this.setState({
+			month: (months.indexOf(e.target.innerText) + 1),
+			showMonth: !showMonth 
+		},
+		() => this.props.today(selectedDay),
+		);
+	}
+
 	render() {
-		const { month, year, months, weekDays, today, tableCells } = this.state;
-		var firstDay = this.firstDay().split(' ');
-		var previousMonthDays = this.getMonthDays(year, month - 1);
+		// console.log(this.febDays,"febDays", this.state, "rndr state...");
+		const { month, year, months, weekDays, today, tableCells, showMonth } = this.state;
+		// to get the first day of month
+		var firstDay = this.getMonthDays(year, (month - 1), 1).split(' ');
+
+		// to get the all days of previous month
+		var previousMonthDays = this.getMonthDays(year, (month - 1));
+		
+		// to get the all days of current month
 		var currentMonthDays = this.getMonthDays(year, month);
-		var nextMonthDays = this.getMonthDays(year, month + 1);
+
+		// to get the all days of next month
+		var nextMonthDays = this.getMonthDays(year, (month + 1));
 		var position = weekDays.indexOf(firstDay[0].toUpperCase());
-		var pastDays = (tableCells - previousMonthDays) - position;
+		var pastDays = (tableCells - currentMonthDays) - position;
 		var nextDays = tableCells - (pastDays + currentMonthDays);
 		var calender = [];
 
+		// console.log(nextMonthDays, pastDays, nextDays, position, "position...");
+
 		// loop to add previous month days into an array
 		while (pastDays > 0) { 
-		  calender.push(previousMonthDays - pastDays + 1);
+		  calender.push(previousMonthDays - (pastDays + 1));
 		  --pastDays;
 		}
 
@@ -116,10 +146,23 @@ class Calender extends Component {
 						<div className="calender-hdr">
 							<span onClick={this.handleClick} data-key="dec-year">{"<<"}</span>
 							<span onClick={this.handleClick} data-key="dec-month">{"<"}</span>
-							<h3>{ months[month] } { year }</h3>
+							<h3>
+								<span className="current-month" onClick={this.handleMonth}>{ months[month - 1] }</span>
+								<span> { year }</span>
+							</h3>
 							<span onClick={this.handleClick} data-key="inc-month">{">"}</span>
 							<span onClick={this.handleClick} data-key="inc-year">{">>"}</span>
 						</div>
+
+						<div className="disply-month">
+							{	
+								!showMonth ? null :
+									months.map((month, index) => (
+										<p key={index} onClick={this.selectMonth}>{month}</p>
+									))
+							}
+						</div>
+
 						<div className="month">
 							<div className="week-days">
 								{
@@ -131,12 +174,16 @@ class Calender extends Component {
 
 							<div className="date-table">
 								{
-									calender.length === tableCells ? calender.map((DATE, idx) => (
+									calender.length /*=== tableCells*/ ? calender.map((DATE, idx) => (
 										<p className={ DATE === this.date.getDate() /*&&  this.date.getDay() === new Date().getDay() */? "day current-day": "day" } onClick={this.handleDay} key={idx}>{ DATE }</p>
 									)) : null
 								}
 							</div>
-							<p className="today" onClick={() => this.props.today(today)}>today</p>
+							<div className="calender-footer">
+								<DateFormat />
+								<p className="today" onClick={() => this.props.today(today)}>today</p>
+
+							</div>
 						</div>
 
 					</div>
