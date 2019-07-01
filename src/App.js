43b15044a74f61da5c9eb43922b5a75d628138fe
sajan <sajanloveu123@gmail.com>
2019-07-01 13:54:03 +0530

    remove date-range component to other rapo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Date from './Date';
-import DateRange from './DateRange';
 
 class App extends Component {
-
-  state = {
-    shwoCalander: false
-  }
   
-  // handleBlur = () => {
-  //   console.log("fired");
-  //   this.setState({ shwoCalander: "hide" });
-  // }
-
   render(){
     return (
       <div className="app" >
         <Date />
-        <DateRange />
       </div>
     )
   }
